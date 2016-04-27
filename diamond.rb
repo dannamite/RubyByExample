402211cfdfdfabe226a09102a466a29f0a059d6d
Dan Boateng <hello.dannamite@gmail.com>
2016-04-27 20:19:07 +0000

    diamond solution added

--- diamond.rb
+++ diamond.rb
@@ -1,28 +1,29 @@
 # Print 7 rows of ‘*’ in a diamond shape as below:
-#    *
-#   ***
-#  *****
-# *******
+#   *
+#  ***
+# *****
+#*******
 #  *****
 #   ***
 #    *
 
 space = " "
 counter = 3
-count = 0
 
-stars_b4 = 
-
-7.times do |rows|
-  if rows < 3 
-
-    puts "*" * (rows - 1 + 1) * 2 + 1
+8.times do |rows|
+  if rows < 3  
+    space = " " * counter
+    stars = "*" * ((rows - 1 + 1) * 2 + 1)
+    puts space + stars
+    counter -= 1
     
   elsif rows == 4
     puts "*" * 7
   elsif rows > 4
-   
-   puts "*" * (7 - rows + 1) * 2 + 1
+   counter +=1
+   space = " " * counter
+   stars = "*" * ((7 - rows) * 2 + 1)
+   puts space + stars
    
   end
 end
